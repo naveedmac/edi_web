40eab47ecb189a952cedc19b0b9fdfc37a9f4665
Naveed Raza <naveedraza@gmail.com>
2021-01-15 09:05:56 -0800

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+# Load the Rails application.
+require_relative "application"
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+
+# if Rails.env.development?
+#     Zeitwerk::Loader.eager_load_all
+# end
